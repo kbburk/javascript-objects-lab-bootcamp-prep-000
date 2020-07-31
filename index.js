b5a95b2fee812eba81a359060c05f5e26b95733f
Kimberly Sampson <kbburk54@yahoo.com>
2020-07-31 06:07:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
+const recipes = {};
 
-const recipes = function(){
+function updateObjectWithKeyAndValue(recipes, key, value)  {
+    recipes = {prop: 1};
+    var newobj = Object.assign({}, recipes, {prop2: 2});
+    return obj;
+   }
+   
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
+  recipes = {prop: 1};
+  recipes.prop2 = 2;
+  return recipes;
+}
+
+function deleteFromObjectByKey(object, key){
+  var recipes = {prop: 2}
+  var newObj = Object.assign({}, recipes)
+  delete newObj.prop
+  return newObject;
+}
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
+  recipes = {prop: 1};
+  delete recipes.prop;
+  return recipes;
+}
+
+
+
+
+/*const recipes = function(){
   var obj = {};
   return obj;
 };
@@ -15,7 +43,7 @@ function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
   
   recipes.prop2 = 2;
   return recipes;
-}
+}*/
 
 
 
